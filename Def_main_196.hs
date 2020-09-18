4d5eec432fcdc3dcaa89180ed05c6349b03a2d32
Lembrary <admin@lembrary.com>
2020-09-18 20:45:21 +0000

    Auto

---
+++ Def_main_196.hs
@@ -0,0 +1,9 @@
+module Def_main_196 where 
+import Network.HTTP.Base
+import Network.HTTP.Headers
+import Network.HTTP
+import Data.List
+import Def_procLine_15
+import Def_ising5_1
+
+main = print (procLine ising5)
