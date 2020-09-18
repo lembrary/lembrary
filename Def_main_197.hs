46144089a65b9d7663b91626bd32b397f6a65cd7
Lembrary <admin@lembrary.com>
2020-09-18 20:58:05 +0000

    Auto

---
+++ Def_main_197.hs
@@ -0,0 +1,9 @@
+module Def_main_197 where 
+import Network.HTTP.Base
+import Network.HTTP.Headers
+import Network.HTTP
+import Data.List
+import Def_procLine_15
+import Def_ising_0
+
+main = print (map procLine ising)
