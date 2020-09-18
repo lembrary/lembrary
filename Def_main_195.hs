ebe5ad276fe4ef58e3ad9cbf6a7c53e4b4884640
Lembrary <admin@lembrary.com>
2020-09-18 20:44:57 +0000

    Auto

---
+++ Def_main_195.hs
@@ -0,0 +1,9 @@
+module Def_main_195 where 
+import Network.HTTP.Base
+import Network.HTTP.Headers
+import Network.HTTP
+import Data.List
+import Def_procLine_15
+import Def_test_1
+
+main = print (procLine test)
