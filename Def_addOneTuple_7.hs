a87d658f0c4cb4bdd77a4620d2cc52c3d83bbd30
Lembrary <admin@lembrary.com>
2020-09-18 21:09:53 +0000

    Auto

---
+++ Def_addOneTuple_7.hs
@@ -0,0 +1,7 @@
+module Def_addOneTuple_7 where 
+import Network.HTTP.Base
+import Network.HTTP.Headers
+import Network.HTTP
+import Data.List
+
+addOneTuple x = (map show $ map (+1) (take 3 x)) ++ [show $ last x] :: [Int]
