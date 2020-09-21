6c972efc0bdc87a234f18155b084505d7377beb7
Lembrary <admin@lembrary.com>
2020-09-21 04:59:01 +0000

    Auto

---
+++ Def_main_1.hs
@@ -0,0 +1,5 @@
+module Def_main_1 where 
+import Def_midpoint_0
+import Def_xs_0
+
+main = print (midpoint xs)
---
+++ Def_main_2.hs
@@ -0,0 +1,5 @@
+module Def_main_2 where 
+import Def_xs_0
+import Def_split_0
+
+main = print (split xs)
