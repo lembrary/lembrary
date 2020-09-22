dadf927bbaa6ba4455a352591b7ecfefaf63328f
Lembrary <admin@lembrary.com>
2020-09-22 22:21:01 +0000

    Auto

--- Def_merge2_0.hs
+++ Def_merge2_0.hs
@@ -1,4 +1,3 @@
 module Def_merge2_0 where 
-import Def_merge_0
 
-merge2 = merge
+merge2 xs ys = case (xs, ys) of ([], _) -> ys; (_, []) -> xs; (x:xt, y:yt) -> if x < y then x:(merge2 xt ys) else y:(merge2 xs yt)
