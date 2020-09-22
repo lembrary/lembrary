600d94823dba04d2b8e23fdb9244161b89f2dfb7
Lembrary <admin@lembrary.com>
2020-09-22 22:05:01 +0000

    Auto

--- Def_main_2.hs
+++ Def_main_2.hs
@@ -1,5 +1,6 @@
 module Def_main_2 where 
 import Def_split_1
-import Def_xs_0
+import Def_testList_0
+import Def_merge2_0
 
-main = print (split xs == [[4,3,1], [5,1,6,4]])
+main = print (merge2 $ split testList)
