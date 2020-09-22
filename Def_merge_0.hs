e287185f1cf69d6b5546e5eb4b2ccac4a3ba4782
Lembrary <admin@lembrary.com>
2020-09-22 22:15:01 +0000

    Auto

--- Def_merge_0.hs
+++ Def_merge_0.hs
@@ -1,3 +1,3 @@
 module Def_merge_0 where 
 
-merge xs ys = case (xs, ys) of ([], _) -> ys; (_, []) -> xs; (x: xt, y: yt) -> if x < y then x:(merge xt ys) else y:(merge xs yt)
+merge xs ys = case (xs, ys) of ([], _) -> ys; (_, []) -> xs; (x:xt, y:yt) -> if x < y then x:(merge xt ys) else y:(merge xs yt)
